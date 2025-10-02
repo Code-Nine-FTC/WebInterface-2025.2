534e7c6bd5e5137f74df14baa46b1d083732f576
OliveiraPedro09 <pedromartinsoliveira9@gmail.com>
2025-10-02 13:42:42 -0300

    feat: add eslint/prettier/husky

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import vue from 'eslint-plugin-vue';
+
+export default [
+  {
+    files: ['**/*.vue'],
+    ignores: ['node_modules', 'dist'],
+    languageOptions: {
+      parser: await import('vue-eslint-parser'),
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        parser: {
+          js: await import('@typescript-eslint/parser'),
+          ts: await import('@typescript-eslint/parser'),
+        },
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    plugins: {
+      vue,
+    },
+    rules: {
+      // Suas regras para Vue aqui
+    },
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules', 'dist'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      // Suas regras JS/TS aqui
+    },
+  },
+];
